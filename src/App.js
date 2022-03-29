1085c190a449c415ac1229f13b476eae1911930a
jackvdp <jack@pumpymusic.co.uk>
2022-03-29 12:34:11 +0100

    Starter react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Accordion from "./components/Accordion"
+import Header from "./components/Header"
+import axios from "axios"
+import React, { useState, useEffect } from 'react';
+import cors from "cors"
 
 function App() {
+
+  const [apiData, setapiData] = useState("")
+  useEffect(() => {
+    axios.get("https://hackney-bookings-api.herokuapp.com/booking")
+    .then(response_from_api => {
+      console.log(response_from_api)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    // Update the document title using the browser API
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Header />
+    <Accordion />
+    </>
+  )
 }
 
-export default App;
+export default App
