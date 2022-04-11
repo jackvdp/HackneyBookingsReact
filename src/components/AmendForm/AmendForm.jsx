6f5b64efc2f0f31694a8d640425aafb08436314b
Yasmin Morshed <snowmoss10@gmail.com>
2022-04-11 14:58:00 +0100

    2 forms on page, starting to use Cypress with eslint

---
+++ src/components/AmendForm/AmendForm.jsx
@@ -0,0 +1,21 @@
+function AmendForm() {
+  return (
+    <div class="govuk-form-group lbh-form-group">
+      {/* Reference number */}
+      <label class="govuk-label lbh-label" for="input-example">
+        Enter your reference number:
+      </label>
+      <span id="input-with-hint-text-hint" class="govuk-hint lbh-hint">
+        This is the reference number you received when you created a booking.
+      </span>
+      <input
+        class="govuk-input lbh-input"
+        id="input-example"
+        name="test-name"
+        type="text"
+      />     
+    </div>
+  )
+}
+
+export default AmendForm
--- src/routes/Amend.jsx
+++ src/routes/Amend.jsx
@@ -1,8 +1,14 @@
+import AmendForm from "../components/AmendForm/AmendForm";
+
 export default function Amend() {
   return (
     <main className="lbh-main-wrapper" id="main-content" role="main">
-    <div className="lbh-container">
-      <h2>Amend your Booking</h2>
+      <div className="lbh-container">
+        <h2>Amend your Booking</h2>
+        <AmendForm />
+        <button class="govuk-button lbh-button" data-module="govuk-button">
+          Save and continue
+        </button>
       </div>
     </main>
   );
