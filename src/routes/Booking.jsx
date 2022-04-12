fb6d80735f5767ed90beba3f6f06c3d177eedb09
Yasmin Morshed <snowmoss10@gmail.com>
2022-04-12 17:36:14 +0100

    End of day 12th April

--- src/routes/Booking.jsx
+++ src/routes/Booking.jsx
@@ -6,14 +6,6 @@ export default function Booking() {
     <main className="lbh-main-wrapper" id="main-content" role="main">
       <div className="lbh-container">
         <h2>Create a Booking</h2>
-        <h5>The spaces available:
-          <br></br>
-          <ul>
-            <li><a href="https://www.hackney.gov.uk/hackney-marshes">Hackney Marshes</a></li>
-            <li><a href="https://www.hackney.gov.uk/mabley-green">Mabley Green</a>&nbsp;– 3G pitch</li>
-            <li><a href="https://www.hackney.gov.uk/haggerston-park">Haggerston Park</a></li>
-          </ul>
-        </h5>
         <BookingForm />
       </div>
     </main>
